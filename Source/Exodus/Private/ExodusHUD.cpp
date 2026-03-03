2609446b3ba6939ccbda9fbbaf895bb229848f99
YoonjaeDong <yjdong114@naver.com>
2026-03-03 09:22:34 +0900

    add : Option, HUD

---
+++ Source/Exodus/Private/ExodusHUD.cpp
@@ -0,0 +1,48 @@
+﻿#include "ExodusHUD.h"
+#include "Components/TextBlock.h"
+#include "TimerManager.h"
+#include "Kismet/GameplayStatics.h" 
+#include "ABaseCharacter.h" 
+
+void UExodusHUD::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+    TotalSeconds = StartTimeSeconds;
+
+    if (Txt_Timer)
+    {
+        int32 Minutes = TotalSeconds / 60;
+        int32 Seconds = TotalSeconds % 60;
+        Txt_Timer->SetText(FText::FromString(FString::Printf(TEXT("%02d:%02d"), Minutes, Seconds)));
+    }
+
+    GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &UExodusHUD::UpdateTimer, 1.0f, true);
+}
+
+void UExodusHUD::UpdateTimer()
+{
+    TotalSeconds--;
+
+    int32 Minutes = TotalSeconds / 60;
+    int32 Seconds = TotalSeconds % 60;
+
+    FString TimeString = FString::Printf(TEXT("%02d:%02d"), Minutes, Seconds);
+
+    if (Txt_Timer)
+    {
+        Txt_Timer->SetText(FText::FromString(TimeString));
+    }
+
+    if (TotalSeconds <= 0)
+    {
+        GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
+
+        ACharacter* PlayerCharacter = UGameplayStatics::GetPlayerCharacter(this, 0);
+
+        if (AABaseCharacter* Hunter = Cast<AABaseCharacter>(PlayerCharacter))
+        {
+            Hunter->Die();
+        }
+    }
+}
---
+++ Source/Exodus/Public/ExodusHUD.h
@@ -0,0 +1,30 @@
+﻿// Copyright © 2026 비전공회담. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ExodusHUD.generated.h"
+
+class UTextBlock;
+
+UCLASS()
+class EXODUS_API UExodusHUD : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeConstruct() override;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* Txt_Timer;
+
+	FTimerHandle TimerHandle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Timer")
+	int32 StartTimeSeconds = 600;
+		
+	int32 TotalSeconds;
+
+	void UpdateTimer();
+};
