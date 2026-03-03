c85517b9388b8ce00a75024e12007c8a37f55a33
YoonjaeDong <yjdong114@naver.com>
2026-03-03 12:57:54 +0900

    add : GameInstance , Update : UI

---
+++ Source/Exodus/Public/ExodusGameInstance.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "ExodusGameInstance.generated.h"
+
+
+UCLASS()
+class EXODUS_API UExodusGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UExodusGameInstance();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sound Settings")
+	float Vol_Master;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sound Settings")
+	float Vol_BGM;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sound Settings")
+	float Vol_Player;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sound Settings")
+	float Vol_Zombie;
+};
